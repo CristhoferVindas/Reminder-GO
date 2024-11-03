9b31c6bdd507c350aa8bafd4b8ec7f2b66cf8ef9
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2024-11-03 02:14:31 -0600

    FEATURE: Add ActivitiesDetails screen to StackCategory navigator with custom header styles

--- app/stackCategory/StackCategory.tsx
+++ app/stackCategory/StackCategory.tsx
@@ -1,19 +1,32 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import Categories from '@/components/category/Categories';
 import Activities from '@/components/activity/Activities';
-import Calendar from '../(tabs)/calendar';
+import ActivitiesDetails from '@/components/activity/ActivitiesDetails';
+import {Activity} from '@/types/Activity.type';
 export type RootStackParamList = {
 	Home: undefined;
 	Activities: {categoryId: number};
+	ActivitiesDetails: {activityId: Activity};
 	Calendar: {categoryId: number};
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
 export function StackCategory() {
 	return (
-		<Stack.Navigator initialRouteName="Home">
+		<Stack.Navigator
+			initialRouteName="Home"
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: '#111827',
+				},
+				headerTintColor: '#fff',
+				headerShown: true,
+				headerTitle: '',
+			}}
+		>
 			<Stack.Screen name="Home" component={Categories} />
 			<Stack.Screen name="Activities" component={Activities} />
+			<Stack.Screen name="ActivitiesDetails" component={ActivitiesDetails} />
 		</Stack.Navigator>
 	);
 }
