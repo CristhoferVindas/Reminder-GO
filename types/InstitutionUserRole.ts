cd0c04c48fae9c69124ae9eefb45c9f1f79eaad8
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2024-11-03 01:48:36 -0600

    ADD: Create institution user role relation type to handle database data

---
+++ types/InstitutionUserRole.ts
@@ -0,0 +1,13 @@
+import {Institution} from '@/types/Institution';
+import {User} from '@/types/User.type';
+import {Role} from 'react-native';
+
+export type Institutions_x_users_x_roles = {
+	id?: number | undefined;
+	institutions_id: number | null;
+	users_id: number;
+	roles_id: number;
+	roles: Role;
+	users: User;
+	institutions: Institution;
+};
