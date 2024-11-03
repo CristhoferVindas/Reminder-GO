9b409371e4f555b25859b6441d43552332420765
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2024-11-03 03:23:37 -0600

    ADD: Add ActivitiesDetails screen component

---
+++ app/ActivitiesDetails.tsx
@@ -0,0 +1,13 @@
+import ActivitiesDetails from '@/components/activity/ActivitiesDetails';
+import React from 'react';
+import {View} from 'react-native';
+
+const ActivitiesDetail = () => {
+	return (
+		<View>
+			<ActivitiesDetails />
+		</View>
+	);
+};
+
+export default ActivitiesDetail;
