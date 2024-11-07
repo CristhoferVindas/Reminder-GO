f1ccd95fef76ba033a373a2f25ed5790b6f1a493
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2024-11-07 03:17:13 -0600

    REFACTOR: Update StackSavedActivities Navigator and Screen Options
    - Renamed `RootSavedActivitiesDetailsStackParamList` to `SavedActivitiesDetailsStackParamList` for clarity.
    - Adjusted screen options to set custom titles and disable animation.
    - Updated screen names and header titles in `Stack.Screen` components for better readability and user experience.

--- app/stackSavedActivities/StackSavedActivities.tsx
+++ app/stackSavedActivities/StackSavedActivities.tsx
@@ -2,12 +2,12 @@ import {createStackNavigator} from '@react-navigation/stack';
 import SavedActivities from '@/components/savedActivities/SavedActivities';
 import {Activity} from '@/types/Activity.type';
 import ActivitiesDetails from '@/components/activity/ActivitiesDetails';
-export type RootSavedActivitiesDetailsStackParamList = {
+export type SavedActivitiesDetailsStackParamList = {
 	SavedActivitiesHome: undefined;
 	ActivitiesDetails: {activityId: Activity};
 };
 
-const Stack = createStackNavigator<RootSavedActivitiesDetailsStackParamList>();
+const Stack = createStackNavigator<SavedActivitiesDetailsStackParamList>();
 export function StackSavedActiviy() {
 	return (
 		<Stack.Navigator
@@ -21,8 +21,23 @@ export function StackSavedActiviy() {
 				headerTitle: '',
 			}}
 		>
-			<Stack.Screen name="SavedActivitiesHome" component={SavedActivities} />
-			<Stack.Screen name="ActivitiesDetails" component={ActivitiesDetails} />
+			<Stack.Screen
+				name="SavedActivitiesHome"
+				component={SavedActivities}
+				options={({route}) => ({
+					title: 'sadasdas',
+					headerTitle: 'Tus Actividades ',
+					animationEnabled: false,
+				})}
+			/>
+			<Stack.Screen
+				name="ActivitiesDetails"
+				component={ActivitiesDetails}
+				options={({route}) => ({
+					headerTitle: route.params.activityId.name,
+					animationEnabled: false,
+				})}
+			/>
 		</Stack.Navigator>
 	);
 }
