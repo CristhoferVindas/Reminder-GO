81b8b9c77aaca44a94a4dc2a2dc6bab482d20ca2
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2024-11-06 19:18:49 -0600

    REFACTOR: Update institution state management with asynchronous functions

--- store/institution.store.ts
+++ store/institution.store.ts
@@ -1,17 +1,24 @@
+import {InstitutionsProvider} from '@/provider/Institution.provider';
 import {Institution} from '@/types/Institution';
 import {create} from 'zustand';
 
 interface InstitutionsStoreState {
 	institution: Institution | null;
 	institutions: Institution[] | null;
-	setInstitutions: (institution: Institution) => Promise<void>;
+	setInstitution: (institution: Institution) => Promise<void>;
+
+	getInstitutions: (active: string) => Promise<void>;
 }
 
 const useInstitutionsStore = create<InstitutionsStoreState>((set) => ({
 	institution: null,
 	institutions: null,
-	setInstitutions: async (institution: Institution) => {
+	setInstitution: async (institution: Institution) => {
 		set((state) => ({...state, institution: institution}));
+	},
+	getInstitutions: async (active: string) => {
+		const institutions = await InstitutionsProvider.getInstitutions(active);
+		set((state) => ({...state, institutions: institutions}));
 	},
 }));
 
