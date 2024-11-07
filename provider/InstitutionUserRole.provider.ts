1017be775cc98ebd7bdd0d2c9ad6197131bef11f
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2024-11-06 19:20:33 -0600

    FEATURE: add createInstitutions_x_users_x_roles function to InstitutionUserRoleProvider

--- provider/InstitutionUserRole.provider.ts
+++ provider/InstitutionUserRole.provider.ts
@@ -35,9 +35,32 @@ async function getInstitutions_x_users_x_rolesByEmail(email: string) {
 		throw error;
 	}
 }
+async function createInstitutions_x_users_x_roles(
+	institutions_x_users_x_roles: Institutions_x_users_x_roles
+) {
+	try {
+		const response = await fetch(
+			`http://${process.env.EXPO_PUBLIC_DIRECCIONIP as string}:3000/api/institutions_X_users_X_roles`,
 
+			{
+				method: 'POST',
+				body: JSON.stringify(institutions_x_users_x_roles),
+			}
+		);
 
+		if (!response.ok) {
+			throw new Error(`Error en la solicitud: ${response.statusText}`);
+		}
+
+		const data = await response.json();
+		return data as Institutions_x_users_x_roles;
+	} catch (error) {
+		console.error('Error obteniendo las Actividades por estado:', error);
+		throw error;
+	}
+}
 export const InstitutionUserRoleProvider = {
+	createInstitutions_x_users_x_roles,
 	getInstitutions_x_users_x_rolesByIdUser,
 	getInstitutions_x_users_x_rolesByEmail,
 };
