60d201edc241f56bb18ca81c49eb9d2f385cfec1
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2024-11-07 03:01:19 -0600

    FEATURE: Add getActivitiesByInstitutionID function to ActivitiesProvider

--- provider/Activities.provider.ts
+++ provider/Activities.provider.ts
@@ -39,7 +39,30 @@ async function getActivitiesByDate(date: Date) {
 		throw error;
 	}
 }
+async function getActivitiesByInstitutionID(active: string, institutionId: string) {
+	try {
+		const response = await fetch(
+			`http://${
+				process.env.EXPO_PUBLIC_DIRECCIONIP as string
+			}:3000/api/activities/institution/${active}/${institutionId}`,
+			{
+				method: 'GET',
+			}
+		);
+
+		if (!response.ok) {
+			throw new Error(`Error en la solicitud: ${response.statusText}`);
+		}
+		const data = await response.json();
+
+		return data as Activity[];
+	} catch (error) {
+		console.error('Error obteniendo las Actividades ID de institución:', error);
+		throw error;
+	}
+}
 export const ActivitiesProvider = {
+	getActivitiesByInstitutionID,
 	getActivitiesByCategoryID,
 	getActivitiesByDate,
 };
