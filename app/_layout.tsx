f8e322d89388e400a7693e8fb082a52ed2db1e37
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2024-11-07 03:22:10 -0600

    REFACTOR: Update RootLayout component for user session handling and push notifications
    - Refactored RootLayout to manage user session storage with SecureStore.
    - Integrated push notification token retrieval and notification listener setup.
    - Improved conditional navigation based on user role and institution status.
    - Removed unnecessary SplashScreen dependency.
    - Updated layout structure to streamline navigation and styling.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,64 @@
-import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
-import {useFonts} from 'expo-font';
-import {Stack} from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import {useEffect} from 'react';
-import 'react-native-reanimated';
-
-import {useColorScheme} from '@/hooks/useColorScheme';
-
-SplashScreen.preventAutoHideAsync();
+import * as SecureStore from 'expo-secure-store';
+import useUsersStore from '@/store/user.store';
+import {User} from '@/types/User.type';
+import {Stack, useRouter} from 'expo-router';
+import {useEffect, useState} from 'react';
+import {
+	registerForPushNotificationsAsync,
+	setNotificationListeners,
+} from '@/notifications/PushNotifications';
 
 export default function RootLayout() {
-	const colorScheme = useColorScheme();
-	const [loaded] = useFonts({
-		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-	});
+	const user = useUsersStore((state) => state.user);
+	const [currentUser, setCurrentUser] = useState<User | null>(null);
+	const [renderKey, setRenderKey] = useState(0);
+	const router = useRouter();
+
+	const storeUser = async (user: User) => {
+		try {
+			await SecureStore.setItemAsync('currentUser', JSON.stringify(user));
+		} catch (error) {
+			console.error('Error guardando el usuario:', error);
+		}
+	};
 
 	useEffect(() => {
-		if (loaded) {
-			SplashScreen.hideAsync();
+		if (user == null) {
+			router.navigate('/');
+		} else {
+			setCurrentUser(user);
+			storeUser(user);
+			setRenderKey((prev) => prev + 1);
+			if (user?.institutions?.id == undefined || user?.roles?.id == 2) {
+				router.navigate('/waitingScreen');
+			} else {
+				router.replace('/(tabs)/');
+			}
 		}
-	}, [loaded]);
+	}, [user]);
 
-	if (!loaded) {
-		return null;
-	}
+	useEffect(() => {
+		const fetchToken = async () => {
+			const token = await registerForPushNotificationsAsync();
+			if (token) {
+			}
+		};
+
+		fetchToken();
+		setNotificationListeners();
+	}, []);
 
 	return (
-		<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-			<Stack>
-				<Stack.Screen name="(tabs)" options={{headerShown: false}} />
-				<Stack.Screen name="+not-found" />
-			</Stack>
-		</ThemeProvider>
+		<Stack
+			key={renderKey}
+			screenOptions={{
+				headerStyle: {backgroundColor: '#808080'},
+				headerTintColor: '#fff',
+				headerShown: false,
+			}}
+		>
+			<Stack.Screen name="(tabs)" options={{}} />
+			<Stack.Screen name="ActivitiesDetails" />
+		</Stack>
 	);
 }
