a771ea8b91c7a922bd455ace3b76ff150a88b47c
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2024-11-07 03:19:02 -0600

    FEAT: Extend StackCalendar Navigator with CalendarActivities and ActivitiesDetails
    - Updated `CalendarStackParamList` to include parameters for `CalendarActivities` (date) and `ActivitiesDetails` (activityId).
    - Added new screens to the `StackCalendar` navigator: `CalendarActivities` and `ActivitiesDetails`.
    - Organized screen imports and parameter types for easier navigation and improved type safety.

--- app/stackCalendar/StackCalendar.tsx
+++ app/stackCalendar/StackCalendar.tsx
@@ -1,9 +1,13 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import CalendarScreen from '@/components/calendar/Calendar';
 import CalendarActivities from '@/components/calendar/CalendarActivities';
+import {Category} from '@/types/Category.type';
+import {Activity} from '@/types/Activity.type';
+import ActivitiesDetails from '@/components/activity/ActivitiesDetails';
 export type CalendarStackParamList = {
 	Calendar: undefined;
 	CalendarActivities: {date: string};
+	ActivitiesDetails: {activityId: Activity};
 };
 
 const Stack = createStackNavigator<CalendarStackParamList>();
@@ -22,6 +26,7 @@ export function StackCalendar() {
 		>
 			<Stack.Screen name="Calendar" component={CalendarScreen} />
 			<Stack.Screen name="CalendarActivities" component={CalendarActivities} />
+			<Stack.Screen name="ActivitiesDetails" component={ActivitiesDetails} />
 		</Stack.Navigator>
 	);
 }
