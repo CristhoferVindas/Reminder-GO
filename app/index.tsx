0a5d674ed4d347e0c9e23694af55eb5243faaa90
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-04-25 16:03:51 -0600

    FEATURE: Configure Google Sign-In and navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,27 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from '@/components/login/LoginGoogle';
 import SignInGoogle from '@/components/signin/signinGoogle';
+import React, { useEffect } from 'react';
+import { NavigationContainerRef } from '@react-navigation/native';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 export type LoginStackParamList = {
 	Login: undefined;
 	SignInGoogle: undefined;
 };
 
+useEffect(() => {
+	GoogleSignin.configure({
+		webClientId: process.env.EXPO_WEB_CLIENT_ID,
+	});
+}, []);
+export function navigate(name: string, params?: object) {
+	if (navigationRef.current) {
+		navigationRef.current.navigate(name, params);
+	} else {
+		console.warn('navigationRef no está listo');
+	}
+}
+export const navigationRef = React.createRef<NavigationContainerRef<any>>();
 const Stack = createStackNavigator<LoginStackParamList>();
 const home = () => {
 	return (
